9af4afc73cc460a93fcf74e5de3907ae7abedd08
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-09-15 18:57:02 +0200

    Implemented swagger integration. fix #18

--- examples/process-application/src/main/kotlin/io/holunda/camunda/taskpool/example/process/rest/ProcessStarterController.kt
+++ examples/process-application/src/main/kotlin/io/holunda/camunda/taskpool/example/process/rest/ProcessStarterController.kt
@@ -1,6 +1,8 @@
 package io.holunda.camunda.taskpool.example.process.rest
 
 import io.holunda.camunda.taskpool.example.process.process.ProcessApproveRequestBean
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -11,8 +13,9 @@ open class ProcessStarterController {
   @Autowired
   lateinit var processApproveRequestBean: ProcessApproveRequestBean
 
+  @ApiOperation("Starts process instances up to the given number")
   @PostMapping("/request/start/{count}")
-  open fun startManyInstances(@PathVariable("count") count: Int): ResponseEntity<List<String>> {
+  open fun startManyInstances(@ApiParam("Total number of instances", defaultValue = "10") @PathVariable("count") count: Int): ResponseEntity<List<String>> {
     val instances = mutableListOf<String>()
     for (numberOfInstances in processApproveRequestBean.countInstances()..count) {
       instances.add(processApproveRequestBean.startProcess().processInstanceId)
@@ -20,18 +23,25 @@ open class ProcessStarterController {
     return ResponseEntity.ok(instances)
   }
 
+  @ApiOperation("Deletes all process instances.")
   @DeleteMapping("/request")
   open fun deleteAllInstances() {
     processApproveRequestBean.deleteAllInstances()
   }
 
+  @ApiOperation("Performs approve request task")
   @PostMapping("/request/{id}/decision/{decision}")
-  open fun approve(@PathVariable("id") id: String, @PathVariable("decision") decision: String) {
+  open fun approve(
+    @ApiParam("Process instance id") @PathVariable("id") id: String,
+    @ApiParam("Decision of the approver", allowableValues = "APPROVE, REJECT, RETURN", required=true) @PathVariable("decision") decision: String) {
     processApproveRequestBean.approve(id, decision)
   }
 
+  @ApiOperation("Performs amend request task")
   @PostMapping("/request/{id}/action/{action}")
-  open fun amend(@PathVariable("id") id: String, @PathVariable("action") action: String) {
+  open fun amend(
+    @ApiParam("Process instance id") @PathVariable("id") id: String,
+    @ApiParam("Decision of the originator", allowableValues = "CANCEL, RESUBMIT", required=true) @PathVariable("action") action: String) {
     processApproveRequestBean.amend(id, action)
   }
 
