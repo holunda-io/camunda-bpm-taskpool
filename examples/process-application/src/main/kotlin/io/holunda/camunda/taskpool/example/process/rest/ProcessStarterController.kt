74fdcee3aa96ff05029f74b89b6658baa98d0de2
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-09-15 19:33:44 +0200

    Minor Swagger API changes

--- examples/process-application/src/main/kotlin/io/holunda/camunda/taskpool/example/process/rest/ProcessStarterController.kt
+++ examples/process-application/src/main/kotlin/io/holunda/camunda/taskpool/example/process/rest/ProcessStarterController.kt
@@ -1,19 +1,21 @@
 package io.holunda.camunda.taskpool.example.process.rest
 
 import io.holunda.camunda.taskpool.example.process.process.ProcessApproveRequestBean
+import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@Api("Process Starter Controller")
 @RestController
 open class ProcessStarterController {
 
   @Autowired
   lateinit var processApproveRequestBean: ProcessApproveRequestBean
 
-  @ApiOperation("Starts process instances up to the given number")
+  @ApiOperation("Starts process instances up to the given number.")
   @PostMapping("/request/start/{count}")
   open fun startManyInstances(@ApiParam("Total number of instances", defaultValue = "10") @PathVariable("count") count: Int): ResponseEntity<List<String>> {
     val instances = mutableListOf<String>()
@@ -29,7 +31,7 @@ open class ProcessStarterController {
     processApproveRequestBean.deleteAllInstances()
   }
 
-  @ApiOperation("Performs approve request task")
+  @ApiOperation("Performs approve request task.")
   @PostMapping("/request/{id}/decision/{decision}")
   open fun approve(
     @ApiParam("Process instance id") @PathVariable("id") id: String,
@@ -37,7 +39,7 @@ open class ProcessStarterController {
     processApproveRequestBean.approve(id, decision)
   }
 
-  @ApiOperation("Performs amend request task")
+  @ApiOperation("Performs amend request task.")
   @PostMapping("/request/{id}/action/{action}")
   open fun amend(
     @ApiParam("Process instance id") @PathVariable("id") id: String,
