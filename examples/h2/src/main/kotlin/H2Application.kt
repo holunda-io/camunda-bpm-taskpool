ebb73af0e42e043c5bf6a5aa8cd9d0c8b781a22f
Jan Galinski <jan.galinski@holisticon.de>
2018-10-25 13:57:17 +0200

    refactor no-package-dirs

--- examples/h2/src/main/kotlin/H2Application.kt
+++ examples/h2/src/main/kotlin/H2Application.kt
@@ -3,28 +3,20 @@ package io.holunda.camunda.tasklist
 import org.h2.tools.Server
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.context.annotation.Configuration
+import org.springframework.boot.runApplication
 import javax.annotation.PostConstruct
 
-fun main(args: Array<String>) {
-  SpringApplication.run(H2Application::class.java, *args)
-}
+fun main(args: Array<String>) = runApplication<H2Application>(*args).let { Unit }
 
 @SpringBootApplication
-open class H2Application() {
-}
+open class H2Application(@Value("\${tcp.port:9092}") val tcpPort: String) {
 
-@Configuration
-open class H2Config(@Value("\${tcp.port:9092}") val tcpPort: String) {
-
-  val logger = LoggerFactory.getLogger(this.javaClass)
+  private val logger = LoggerFactory.getLogger(this.javaClass)
 
   @PostConstruct
   open fun init(): Server {
     logger.info("Enabling TCP-Port 9092 of H2")
     return Server.createTcpServer("-tcpPort", tcpPort, "-tcpAllowOthers").start()
   }
-
 }
