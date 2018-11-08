91ca7212af3f6bba88b5ca8a660f48890c894fe9
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-11-08 14:08:25 +0100

    Improved application runner

--- view/simple/src/main/kotlin/io/holunda/camunda/taskpool/view/simple/TaskPoolSimpleViewConfiguration.kt
+++ view/simple/src/main/kotlin/io/holunda/camunda/taskpool/view/simple/TaskPoolSimpleViewConfiguration.kt
@@ -1,23 +1,17 @@
 package io.holunda.camunda.taskpool.view.simple
 
 import io.holunda.camunda.taskpool.view.simple.service.TaskPoolService
-import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
-import org.springframework.stereotype.Component
 
 @ComponentScan
 @Configuration
-open class TaskPoolSimpleViewConfiguration
+open class TaskPoolSimpleViewConfiguration {
 
-
-@Component
-class Initializer(
-  private val taskPoolService: TaskPoolService
-) : ApplicationRunner {
-
-  override fun run(args: ApplicationArguments) {
+  @Bean
+  open fun initializeSimpleView(taskPoolService: TaskPoolService) = ApplicationRunner {
     taskPoolService.restore()
   }
 }
