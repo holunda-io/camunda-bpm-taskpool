60ce2ef62e6c7576bbc24b5390eb403f6d94ceac
Simon Zambrovski <simon.zambrovski@holisticon.de>
2018-12-11 12:36:34 +0100

    chore: add pupeteer, increment versions in package.json

--- view/cockpit/frontend/src/karma.conf.js
+++ view/cockpit/frontend/src/karma.conf.js
@@ -3,6 +3,10 @@
 
 const path = require('path');
 
+// distribute chrome headless via NPM
+process.env.CHROME_BIN = require('puppeteer').executablePath();
+
+
 module.exports = function (config) {
   config.set({
     basePath: '',
