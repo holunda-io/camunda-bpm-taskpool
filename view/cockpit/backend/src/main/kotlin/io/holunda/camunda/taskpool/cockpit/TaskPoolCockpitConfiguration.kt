089328cbee891a5b6467d76643f97ddfeedb82ee
Simon Zambrovski <simon.zambrovski@holisticon.de>
2019-01-16 00:11:06 +0100

    feat: subscribing query

--- view/cockpit/backend/src/main/kotlin/io/holunda/camunda/taskpool/cockpit/TaskPoolCockpitConfiguration.kt
+++ view/cockpit/backend/src/main/kotlin/io/holunda/camunda/taskpool/cockpit/TaskPoolCockpitConfiguration.kt
@@ -3,6 +3,7 @@ package io.holunda.camunda.taskpool.cockpit
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
+
 @ComponentScan
 @Configuration
 open class TaskPoolCockpitConfiguration
