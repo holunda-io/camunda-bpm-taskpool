4c47fa328a8322b3b1298752ce3eb238ce8a1c4e
Simon Zambrovski <simon.zambrovski@holisticon.de>
2019-03-07 10:46:59 +0100

    bugfix: fix autoconfiguration in case the taskpool is disabled.

--- engine/taskpool-collector/src/main/kotlin/io/holunda/camunda/taskpool/FallbackTaskCollectorConfiguration.kt
+++ engine/taskpool-collector/src/main/kotlin/io/holunda/camunda/taskpool/FallbackTaskCollectorConfiguration.kt
@@ -5,10 +5,12 @@ package io.holunda.camunda.taskpool
 import io.holunda.camunda.taskpool.enricher.ProcessVariablesCorrelator
 import io.holunda.camunda.taskpool.enricher.ProcessVariablesFilter
 import io.holunda.camunda.taskpool.urlresolver.TasklistUrlResolver
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 
 /**
+ * No @Configuration required.
  * Configuration used via auto-configuration.
  */
 @ConditionalOnMissingBean(ProcessVariablesFilter::class)
@@ -21,6 +23,7 @@ open class FallbackProcessVariablesFilterConfiguration {
 }
 
 /**
+ * No @Configuration required.
  * Configuration used via auto-configuration.
  */
 @ConditionalOnMissingBean(ProcessVariablesCorrelator::class)
@@ -34,15 +37,17 @@ open class FallbackProcessVariablesCorrelatorConfiguration {
 
 
 /**
+ * No @Configuration required.
  * Configuration used via auto-configuration.
  */
 @ConditionalOnMissingBean(TasklistUrlResolver::class)
 open class FallbackTasklistUrlResolverConfiguration {
+
   /**
    * Property-based Tasklist URL resolver.
    */
   @Bean
-  @ConditionalOnMissingBean
+  @ConditionalOnBean(TaskCollectorProperties::class)
   open fun propertyBasedTasklistUrlResolver(properties: TaskCollectorProperties): TasklistUrlResolver {
     return if (properties.tasklistUrl == null) {
       throw IllegalStateException("Either set camunda.taskpool.collector.tasklist-url property or provide own implementation of TasklistUrlResolver")
@@ -52,5 +57,4 @@ open class FallbackTasklistUrlResolverConfiguration {
       }
     }
   }
-
 }
