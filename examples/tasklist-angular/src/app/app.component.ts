8450c962e608b32c00dc9ab9a8c4820781a52640
Simon Zambrovski <simon.zambrovski@holisticon.de>
2019-03-20 10:13:02 +0100

    feat: integrate more elements into menu

--- examples/tasklist-angular/src/app/app.component.ts
+++ examples/tasklist-angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import { ProfileService } from 'tasklist/services';
+import { UserProfile } from 'tasklist/models';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +9,44 @@ import {Component} from '@angular/core';
   styleUrls: [ 'app.component.scss' ]
 })
 export class AppComponent {
+
+  private userProfile: BehaviorSubject<UserProfile> = new BehaviorSubject<UserProfile>({username: ''});
+
+  constructor(private profileService: ProfileService) {
+    this.subscribe();
+  }
+
+  get user(): User {
+    const username = this.userProfile.getValue().username;
+    return {
+      username: username,
+      avatar: 'https://mdbootstrap.com/img/Photos/Avatars/avatar-13.jpg',
+      fullname: this.capitalize(username)
+    };
+  }
+
+  private subscribe() {
+    this.profileService.getProfile().subscribe(
+      (profile: UserProfile) => {
+        this.userProfile.next(profile);
+        console.log('Loaded user profile for user', profile.username);
+      },
+      (error) => {
+        console.log('Error loading user profile', error);
+      }
+    );
+  }
+
+  private capitalize(value: string): string {
+    if (value == null) {
+      return null;
+    }
+    return value.charAt(0).toUpperCase() + value.substring(1, value.length);
+  }
+}
+
+export class User {
+  fullname: string;
+  avatar: string;
+  username: string;
 }
