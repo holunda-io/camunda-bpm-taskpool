3e509891fa59b625fc41c49b021962981ecc55fe
Lukas Taake <lukas.taake@holisticon.de>
2019-06-20 15:32:52 +0200

    Use UUIDs for user identifiers

--- examples/taskpool-application/src/main/kotlin/io/holunda/camunda/taskpool/example/process/service/SimpleUserService.kt
+++ examples/taskpool-application/src/main/kotlin/io/holunda/camunda/taskpool/example/process/service/SimpleUserService.kt
@@ -1,6 +1,5 @@
 package io.holunda.camunda.taskpool.example.process.service
 
-import io.holunda.camunda.taskpool.example.tasklist.auth.CurrentUserService
 import io.holunda.camunda.taskpool.example.tasklist.auth.UserStoreService
 import io.holunda.camunda.taskpool.view.auth.User
 import io.holunda.camunda.taskpool.view.auth.UserService
@@ -10,12 +9,12 @@ import org.springframework.stereotype.Component
 class SimpleUserService : UserService, UserStoreService {
 
   private val muppetUsers = mutableMapOf(
-    "Ironman" to User("ironman", setOf("avengers")),
-    "Hulk" to User("hulk", setOf("avengers")),
-    "Piggy" to User("piggy", setOf("muppetshow")),
-    "Kermit" to User("kermit", setOf("muppetshow")),
-    "Gonzo" to User("gonzo", setOf("muppetshow")),
-    "Fozzy" to User("fozzy", setOf("muppetshow"))
+    "5939c52b-d560-4c72-b6e5-61b7996361c4" to User("ironman", setOf("avengers")),
+    "664313ad-58e0-46d8-a186-e9654bad845f" to User("hulk", setOf("avengers")),
+    "98496787-eb18-4fde-9075-a82194132efd" to User("piggy", setOf("muppetshow")),
+    "37bff195-06fe-4788-9480-a7d9ac6474e1" to User("kermit", setOf("muppetshow")),
+    "27b8aa55-42a2-407a-be9d-d956188aa334" to User("gonzo", setOf("muppetshow")),
+    "394ffece-9412-4ba6-8b3e-57236c356881" to User("fozzy", setOf("muppetshow"))
   )
 
   override fun getUser(userIdentifier: String): User {
@@ -26,4 +25,3 @@ class SimpleUserService : UserService, UserStoreService {
 
   override fun getUserIdentifiers(): List<String> = muppetUsers.keys.toList()
 }
-
