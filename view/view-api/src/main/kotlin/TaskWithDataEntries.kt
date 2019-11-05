551a68cb55396be2d492feea15d6a4b876875888
Simon Zambrovski <simon.zambrovski@holisticon.de>
2019-11-05 17:13:18 +0100

    more docs

--- view/view-api/src/main/kotlin/TaskWithDataEntries.kt
+++ view/view-api/src/main/kotlin/TaskWithDataEntries.kt
@@ -1,10 +1,22 @@
 package io.holunda.camunda.taskpool.view
 
+/**
+ * Represents a joined entity of a task with correlated data entries.
+ */
 data class TaskWithDataEntries(
+  /**
+   * Task from the engine.
+   */
   val task: Task,
+  /**
+   * List of data entries, bound using task correlation ids.
+   */
   val dataEntries: List<DataEntry> = listOf()
 ) {
   companion object {
+    /**
+     * Helper function to correlate.
+     */
     fun correlate(task: Task, dataEntries: List<DataEntry>): TaskWithDataEntries =
       TaskWithDataEntries(
         task = task,
@@ -15,11 +27,17 @@ data class TaskWithDataEntries(
         }.toList()
       )
 
+    /**
+     * Helper function to correlate.
+     */
     fun correlate(tasks: List<Task>, dataEntry: DataEntry): List<TaskWithDataEntries> =
       tasks.filter { it.correlationIdentities.contains(dataEntry.identity) }
         .map { TaskWithDataEntries(it, listOf(dataEntry)) }
         .toList()
 
+    /**
+     * Helper function to correlate.
+     */
     fun correlate(tasks: List<Task>, dataEntries: List<DataEntry>) =
       tasks.map { correlate(it, dataEntries) }
   }
