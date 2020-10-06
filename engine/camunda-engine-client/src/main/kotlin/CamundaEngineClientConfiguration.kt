74774ebd9addb5f8b36812c4fb0ea28171c78055
Simon Zambrovski <simon.zambrovski@holisticon.de>
2020-10-06 19:26:07 +0200

    more javadoc

--- engine/camunda-engine-client/src/main/kotlin/CamundaEngineClientConfiguration.kt
+++ engine/camunda-engine-client/src/main/kotlin/CamundaEngineClientConfiguration.kt
@@ -3,7 +3,9 @@ package io.holunda.camunda.client
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
-
+/**
+ * Engine client configuration enabling the event handling of interaction commands.
+ */
 @Configuration
 @ComponentScan
 class CamundaEngineClientConfiguration
