cf8b661ef5bdf84ebda3b4c5f3f9ec974aa3fcb9
Simon Zambrovski <simon.zambrovski@holisticon.de>
2021-04-28 17:43:51 +0200

    fix the update/create enrichment, fix #322

--- engine/taskpool-collector/src/main/kotlin/io/holunda/camunda/taskpool/enricher/ProcessVariablesTaskCommandEnricher.kt
+++ engine/taskpool-collector/src/main/kotlin/io/holunda/camunda/taskpool/enricher/ProcessVariablesTaskCommandEnricher.kt
@@ -4,6 +4,8 @@ import io.holunda.camunda.taskpool.api.task.*
 import io.holunda.camunda.taskpool.callInProcessEngineContext
 import mu.KLogging
 import org.camunda.bpm.engine.RuntimeService
+import org.camunda.bpm.engine.TaskService
+import org.camunda.bpm.engine.variable.VariableMap
 import org.camunda.bpm.engine.variable.Variables.createVariables
 
 /**
@@ -13,23 +15,51 @@ import org.camunda.bpm.engine.variable.Variables.createVariables
  */
 open class ProcessVariablesTaskCommandEnricher(
   private val runtimeService: RuntimeService,
+  private val taskService: TaskService,
   private val processVariablesFilter: ProcessVariablesFilter,
   private val processVariablesCorrelator: ProcessVariablesCorrelator,
 ) : VariablesEnricher {
 
   companion object : KLogging()
 
+  /**
+   * Retrieves typed variables to enrich current command.
+   * @param command command to enrich.
+   * @return variable map.
+   */
+  open fun <T : TaskIdentityWithPayloadAndCorrelations> getTypeVariables(command: T): VariableMap {
+    return if (command.isHistoric()) {
+      // Task updated
+      // This is a historic command which is processed from a command context listener on command context close. Accessing variables at this point will try to add
+      // another command context listener (some camunda feature about updating mutable variables without explicitly saving them), which in turn causes a
+      // ConcurrentModificationException.
+      // We work around this by opening a new context.
+      // Caution: This will only work if the task is already flushed to the database, e.g. if it was created in a previous transaction.
+      // It will also see only the state of the variables that is flushed to the database
+      callInProcessEngineContext(newContext = true) {
+        val task = taskService.createTaskQuery().taskId(command.id).singleResult()
+        if (task != null) {
+          taskService.getVariablesTyped(command.id)
+        } else {
+          val execution = runtimeService.createExecutionQuery().executionId(command.sourceReference.executionId).singleResult()
+          if (execution != null) {
+            runtimeService.getVariablesTyped(command.sourceReference.executionId)
+          } else {
+            logger.debug { "ENRICHER-004: Could not enrich variables from running execution ${command.sourceReference.executionId}, since it doesn't exist (anymore)." }
+            createVariables()
+          }
+        }
+      }
+    } else {
+      // Create task
+      taskService.getVariablesTyped(command.id)
+    }
+  }
+
   override fun <T : TaskIdentityWithPayloadAndCorrelations> enrich(command: T): T {
 
-    val variablesTyped = callInProcessEngineContext(command.isHistoric()) {
-      val execution = runtimeService.createExecutionQuery().executionId(command.sourceReference.executionId).singleResult()
-      if (execution != null) {
-        runtimeService.getVariablesTyped(command.sourceReference.executionId)
-      } else {
-        logger.debug { "ENRICHER-004: Could not enrich variables from running execution ${command.sourceReference.executionId}, since it doesn't exist (anymore)." }
-        createVariables()
-      }
-    }
+    // load variables typed
+    val variablesTyped = getTypeVariables(command)
 
     // Payload enrichment
     command.payload.putAllTyped(
