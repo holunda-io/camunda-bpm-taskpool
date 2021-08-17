0fea96169508c9dfc57af1ded063e71e65c188b5
Simon Zambrovski <simon.zambrovski@holisticon.de>
2021-08-17 10:01:07 +0200

    more docs, killed some codacy warnings

--- view/jpa/src/main/kotlin/io/holunda/polyflow/view/jpa/data/PayloadAttribute.kt
+++ view/jpa/src/main/kotlin/io/holunda/polyflow/view/jpa/data/PayloadAttribute.kt
@@ -4,6 +4,9 @@ import java.io.Serializable
 import javax.persistence.Column
 import javax.persistence.Embeddable
 
+/**
+ * Represents a searchable payload attribute consisting of the (JSON)-path pf the attribute and its value.
+ */
 @Embeddable
 class PayloadAttribute(
   @Column(name = "PATH", nullable = false)
@@ -13,6 +16,10 @@ class PayloadAttribute(
 ) : Serializable {
 
   companion object {
+    /**
+     * Factory method to create payload attributes out of map entries.
+     * Check VariableSerializer methods how such map entries can be created.
+     */
     operator fun invoke(entry: Map.Entry<String, Any>) = PayloadAttribute(path = entry.key, value = entry.value.toString())
   }
 
