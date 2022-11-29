b9eab43cb9bfb85e6fedc018616ccec3d9d7c0ac
Simon Zambrovski <simon.zambrovski@holisticon.de>
2022-11-29 01:52:24 +0100

    try subscribing

--- view/jpa/src/test/kotlin/io/holunda/polyflow/view/jpa/itest/TestApplicationDataJpa.kt
+++ view/jpa/src/test/kotlin/io/holunda/polyflow/view/jpa/itest/TestApplicationDataJpa.kt
@@ -1,16 +1,10 @@
 package io.holunda.polyflow.view.jpa.itest
 
-import org.axonframework.eventhandling.tokenstore.jpa.TokenEntry
-import org.axonframework.modelling.saga.repository.jpa.SagaEntry
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Import
 
 @SpringBootApplication
 @Import(value = [ObjectMapperConfiguration::class, MockQueryEmitterConfiguration::class])
 @ComponentScan(basePackages = ["io.holunda.polyflow.view.jpa"])
-@EntityScan(
-  basePackageClasses = [TokenEntry::class, SagaEntry::class]
-)
 class TestApplicationDataJpa
