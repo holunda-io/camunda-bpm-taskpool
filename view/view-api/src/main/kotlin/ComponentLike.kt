744e01b4ba9c596355c287efa8dc4662a57fcfd5
Simon Zambrovski <simon.zambrovski@holisticon.de>
2023-10-18 10:29:24 +0200

    add marker interface to open components, fix #888

---
+++ view/view-api/src/main/kotlin/ComponentLike.kt
@@ -0,0 +1,8 @@
+package io.holunda.polyflow.view
+
+/**
+ * Marker annotation to open the classes in Kotlin.
+ */
+@MustBeDocumented
+@Target(AnnotationTarget.CLASS, AnnotationTarget.ANNOTATION_CLASS)
+annotation class ComponentLike
