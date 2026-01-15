758190a19d7226244af6d56d749e496b4351e98e
S-Tim <tim.silhan@holisticon.de>
2026-01-15 17:51:48 +0100

    #1183: Implement involvements only flag (#1199)
    * feat(#1183): Implement involvements only flag
    
    Added a new flag to filter tasks by involvement in any correlated data entry. This flag can be used in combination with the assignedToMe flag. The result is then combined with a logical OR. So both assigned and involved tasks are returned.
    
    * feat(#1183): Changed inner (default) joins to left joins
    
    Combining specifications with composeOr could lead to fewer results (yes, really) because the join statements in the composed specifications are lifted to the root level. When using inner joins this already filters part of the result.
    
    * feat(#1183): tests and cleanup
    
    * chore(#1183): formatting
    
    * chore(#1183): fix test

--- view/view-api/src/main/kotlin/query/task/TasksForUserQuery.kt
+++ view/view-api/src/main/kotlin/query/task/TasksForUserQuery.kt
@@ -7,6 +7,7 @@ import io.holunda.polyflow.view.auth.User
  * Query for tasks visible for user.
  * @param user - the user with groups accessing the tasks.
  * @param assignedToMeOnly flag indicating if the resulting tasks must be assigned to the user only.
+ * @param involvementsOnly flag indicating if the results should only contain tasks where the user is involved in any of the correlated data entries
  * @param page - page to read, zero-based index.
  * @param size - size of the page
  * @param sort - optional attribute to sort by.
@@ -15,42 +16,12 @@ import io.holunda.polyflow.view.auth.User
 data class TasksForUserQuery(
   val user: User,
   val assignedToMeOnly: Boolean = false,
+  val involvementsOnly: Boolean = false,
   override val page: Int = 0,
   override val size: Int = Int.MAX_VALUE,
   override val sort: List<String> = listOf(),
   override val filters: List<String> = listOf()
 ) : PageableSortableFilteredTaskQuery {
-
-  @Deprecated("Please use other constructor setting sort as List<String>")
-  constructor(user: User, assignedToMeOnly: Boolean = false, page: Int = 0, size: Int = Int.MAX_VALUE, sort: String?, filters: List<String> = listOf()): this(
-    user = user,
-    assignedToMeOnly = assignedToMeOnly,
-    page = page,
-    size = size,
-    sort = if (sort.isNullOrBlank()) {
-      listOf()
-    } else {
-      listOf(sort)
-    },
-    filters = filters
-  )
-
-  /**
-   * Compatibility constructor for old clients.
-   */
-  @Deprecated(message = "Please use other constructor setting the assignedToMeOnly.")
-  constructor(user: User, page: Int = 0, size: Int = Int.MAX_VALUE, sort: String? = null, filters: List<String> = listOf()): this(
-    user = user,
-    assignedToMeOnly = false,
-    page = page,
-    size = size,
-    sort = if (sort.isNullOrBlank()) {
-      listOf()
-    } else {
-      listOf(sort)
-    },
-    filters = filters
-  )
 
   override fun applyFilter(element: Task): Boolean =
     if (assignedToMeOnly) {
