b8d9dad6a4bdf4ae68974b4bc78ed410a5881e0e
Michael von Bargen <michaelvb93@gmail.com>
2026-09-11 17:49:11 +0200

    #1263: replace toFuture() with toNonNullFuture() for better null handling

--- integration/common/liquibase/src/main/kotlin/io/holunda/polyflow/liquibase/autoconfigure/PolyflowLiquibaseAdoptionAutoConfiguration.kt
+++ integration/common/liquibase/src/main/kotlin/io/holunda/polyflow/liquibase/autoconfigure/PolyflowLiquibaseAdoptionAutoConfiguration.kt
@@ -12,11 +12,11 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties
 import org.springframework.context.ConfigurableApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration
+import org.springframework.boot.liquibase.autoconfigure.LiquibaseProperties
 import org.springframework.core.io.ResourceLoader
 import java.util.Properties
 import javax.sql.DataSource
